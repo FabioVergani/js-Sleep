4891608e103e04c6665f736583346c640239a311
FabioVergani <fabio.vergani.1975@gmail.com>
2014-08-24 18:24:06 +0200

    Update sleep.js

--- sleep.js
+++ sleep.js
@@ -1,2 +1,5 @@
-function sleep(t,f){var e=setInterval(function(){if(++o.j>=1){clearInterval(o.i);f()}},t),o={j:0,i:e}};
+
+function sleep(t,f){var o={j:0,i:setInterval(function(){if(++o.j>=1){clearInterval(o.i);f()}},t)}};
 sleep(500,function(){console.log('dontsleep!')});
+
+
